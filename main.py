f7dc47963396e37b3c73007a29276265dd7faf03
gosnitty <tkachyshyn.pn@ucu.edu.ua>
2023-02-21 22:44:22 +0200

    function read_file done

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+'''
+imports modules
+'''
+from math import radians, cos, sin, asin, sqrt
+import argparse
+from geopy.geocoders import Nominatim
+import folium
 
+def read_file(path):
+    '''
+    this function reads file
+    '''
+    file = open(path, 'r', encoding= 'UTF-8')
+    file = file.read().strip().split('\n')
+    file = [i.split('\t') for i in file]
+    return file[10:]
