d10f9bb6340c1979f4312e8e2be70ed2171cb537
gosnitty <tkachyshyn.pn@ucu.edu.ua>
2023-02-21 22:50:18 +0200

    function right year done

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+def right_year(path, year):
+    '''
+    this function returns list with films of certain year
+    '''
+    file = [i for i in read_file(path) for j in i if year in j ]
+    return file
 
